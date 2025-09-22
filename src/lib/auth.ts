331c2589a8eeb5b6390f281fa004f39b47071986
fedaykin-adel <gabriel_adelar@hotmail.com>
2025-09-21 23:45:38 -0300

    bla

---
+++ src/lib/auth.ts
@@ -0,0 +1,17 @@
+import {} from "@/generated/prisma";
+import {} from "crypto";
+import { jwtVerify } from "jose";
+
+const SECRET = process.env.AUTH_SECRET || "dev-secret-change-me";
+const encoder = new TextEncoder();
+
+export async function verifyJWT(token: string) {
+  const { payload } = await jwtVerify(token, encoder.encode(SECRET));
+  return payload as {
+    sub: string;
+    email: string;
+    name: string;
+    iat: number;
+    exp: number;
+  };
+}
